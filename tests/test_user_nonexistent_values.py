f96ceea49b9350103972997a97dc6f688c071dd9
Shemetova Anastasiya <51288093+asya474@users.noreply.github.com>
2024-09-15 01:32:46 +0700

    advanced_rest_api

--- tests/test_user_nonexistent_values.py
+++ tests/test_user_nonexistent_values.py
@@ -1,7 +1,11 @@
 import pytest
 import requests
 from http import HTTPStatus
+
+from model.reqres import Reqres
+
+
 @pytest.mark.parametrize("user_id", [99])
-def test_user_nonexistent_values(app_url, user_id):
-    response = requests.get(f"{app_url}/api/users/{user_id}")
-    assert response.status_code == HTTPStatus.NOT_FOUND
+def test_user_nonexistent_values(env, user_id):
+    result_response_user_nonexistent_values = Reqres(env).get_user({user_id})
+    assert result_response_user_nonexistent_values.status_code == HTTPStatus.NOT_FOUND
