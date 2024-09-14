9799cc404b2f23ab4177e43124fc18757f852133
Shemetova Anastasiya <51288093+asya474@users.noreply.github.com>
2024-09-15 00:49:56 +0700

    advanced_rest_api

--- model/response_update_user.py
+++ model/response_update_user.py
@@ -1,25 +1,16 @@
+from datetime import datetime
+
 from response_user import ResponseUser
 class ResponseUpdateUser:
     def __init__(self, **kwargs):
-        response = kwargs.pop("response", None)
-        json_ = kwargs.pop("json", response.json() if response else None)
-        self._data = kwargs.pop("data", ResponseUser()).json
+        json_ = kwargs.pop("json", {})
+
         self._json = json_ if json_ else {
-            "data": self._data,
-            "support": {
-                "url": "https://reqres.in/#support-heading",
-                "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
-            }
+        "name": "morpheus",
+        "job": "zion resident",
+        "updatedAt": datetime.datetime.now(datetime.UTC)
         }
 
     @property
     def json(self):
         return self._json
-
-    @property
-    def support_url(self):
-        return self._json['support']['url']
-
-    @property
-    def support_text(self):
-        return self._json['support']['text']
--- tests/test_update_user.py
+++ tests/test_update_user.py
@@ -1,25 +1,21 @@
 import json
-import pytest
-import requests
-from jsonschema import validate
-from http import HTTPStatus
-from random import randint
-from model.reqres import ResponseGetUser, User, ResponseUser, Reqres, UserCreate, UserUpdate
-from faker import Faker
+from datetime import datetime
 
-fake = Faker()
-def test_update_user(app_url):
-    data = {"first_name": fake.first_name(),
-            "last_name": fake.last_name(),
-            "email": fake.free_email(),
-            "avatar": f"https://reqres.in/img/faces/{randint(1, 1000)}-image.jpg"}
-    response = requests.post(url=f"{app_url}/api/users/", data=json.dumps(data))
-    user_id = response.json()["id"]
-    data_update = {"first_name": fake.first_name(),
-                   "last_name": fake.last_name(),
-                   "email": fake.free_email(),
-                   "avatar": f"https://reqres.in/img/faces/{randint(1, 1000)}-image.jpg"}
-    response = requests.patch(url=f"{app_url}/api/users/{user_id}", data=json.dumps(data_update))
-    assert response.status_code == HTTPStatus.OK
-    user = response.json()
-    UserUpdate.model_validate(user)
+import requests
+from http import HTTPStatus
+from model.reqres import Reqres
+from model.response_update_user import ResponseUpdateUser
+from model.response_user import ResponseUser
+
+
+def test_update_user(env):
+
+    expected_response_update_user = ResponseUpdateUser(data=ResponseUpdateUser(
+        name="morpheus",
+        job="zion resident",
+        updatedAt=datetime.datetime.now(datetime.UTC)
+    ))
+
+    result_response_update_user = Reqres(env).update_user(2)
+    assert result_response_update_user.status_code == HTTPStatus.OK
+    assert result_response_update_user.json == expected_response_update_user.json
